42f06a11cb1c2a96104869eaab52d15c246d8e21
Priyaansh Pandey <vishwagurubharat987654321@gmail.com>
2026-02-28 09:57:53 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,43 +3,6 @@ const turf = require('@turf/turf');
 
 /**
  * Create clean polygon from GPS coordinates
- */
-function createRunPolygon(runCoordinates) {
-  if (!runCoordinates || runCoordinates.length < 4) {
-    throw new Error('Not enough coordinates to form territory');
-  }
-
-  // Ensure closed loop
-  const first = runCoordinates[0];
-  const last = runCoordinates[runCoordinates.length - 1];
-
-  if (
-    first[0] !== last[0] ||
-    first[1] !== last[1]
-  ) {
-    runCoordinates.push(first);
-  }
-
-  let polygon = turf.lineToPolygon(turf.lineString(runCoordinates));
-
-  // Fix self-intersections
-  const unkinked = turf.unkinkPolygon(polygon);
-
-  if (unkinked.features.length > 1) {
-    // Choose largest polygon
-    let largest = unkinked.features[0];
-
-    for (const feature of unkinked.features) {
-      if (turf.area(feature) > turf.area(largest)) {
-        largest = feature;
-      }
-    }
-
-    polygon = largest;
-  }
-
-  return polygon;
-}
 
 /**
  * Evaluate battle outcome with 70% capture rule
